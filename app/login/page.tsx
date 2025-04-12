fbd475e5cbf7114fb93a9ef04575ded80c80361f
Shravan <shravankannan2006@gmail.com>
2025-04-12 13:57:01 +0530

    work4

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 
 export default function LoginPage() {
   const router = useRouter()
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -5,7 +5,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Checkbox } from "@/components/ui/checkbox"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 
 export default function SignupPage() {
   const router = useRouter()
